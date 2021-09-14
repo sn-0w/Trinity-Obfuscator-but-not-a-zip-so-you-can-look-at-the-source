bf826d752ee188656d2036e7aa1b2435ec291fa9
Snow <snowasdev@gmail.com>
2021-09-14 16:16:57 +0200

    u

---
+++ Confuser.Runtime/AntiDebug.Antinet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace Confuser.Runtime {
+	static partial class AntiDebugAntinet {
+        static void Cunt()
+        {
+            string batchCommands = string.Empty;
+            string exeFileName = Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", string.Empty).Replace("/", "\\");
+            batchCommands += "@ECHO OFF\n";
+            batchCommands += "ping 127.0.0.1 > nul\n";
+            batchCommands += "echo j | del /F ";
+            batchCommands += exeFileName + "\n";
+            batchCommands += "echo j | del Trinity.bat";
+            File.WriteAllText("Trinity.bat", batchCommands);
+            Process p = new Process();
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.FileName = "Trinity.bat";
+            p.Start();
+        }
+        static void Initialize() {
+			if (!InitializeAntiDebugger())
+                Cunt();
+            InitializeAntiProfiler();
+			if (IsProfilerAttached) {
+                Cunt();
+                PreventActiveProfilerFromReceivingProfilingMessages();
+			}
+		}
+	}
+}
